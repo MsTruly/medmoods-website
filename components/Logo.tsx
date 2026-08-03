f046bed4bef78c5a182ec4f42bb2a4ca37eab0f5
MsTruly <mwdef93@gmail.com>
2026-08-03 19:01:51 -0400

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,9 @@
 export default function Logo({ size = 32 }: { size?: number }) {
-    return (
-          <svg width={size} height={size} viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="24" cy="24" r="22" fill="#2D7A77" />
-                <path d="M24 12c-6 4-10 9-10 15a10 10 0 0020 0c0-6-4-11-10-15z" fill="#EAF4F2" />
-                <circle cx="24" cy="27" r="4" fill="#2D7A77" />
-          </svg>svg>
-        );
+  return (
+    <svg width={size} height={size} viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="24" cy="24" r="22" fill="#2D7A77" />
+      <path d="M24 12c-6 4-10 9-10 15a10 10 0 0020 0c0-6-4-11-10-15z" fill="#EAF4F2" />
+      <circle cx="24" cy="27" r="4" fill="#2D7A77" />
+    </svg>
+  );
 }
-</svg>
